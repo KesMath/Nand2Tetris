100a1118282f7bbbad210462dac83762f8c7a8b5
Kesler Mathieu <kmat5@icloud.com>
2022-12-13 21:03:59 -0500

    successfully got each character to print

--- projects/06/assembler/driver/assembler.cc
+++ projects/06/assembler/driver/assembler.cc
@@ -22,9 +22,15 @@ int main(int argc, char *argv[])
 
     // NOTE: we cannot assume the number of characters (or size of bytes) per line in order to allocate on to a buffer in advance with fgets() approach
     // so we must use fgetc() (supplemented with a counter) for safer approach until NEWLINE is met
-    while(char ch = fgetc(assembly_file) != EOF){
-        int newlineOffset = indexOfNewLine(assembly_file);
+    void* buffer = NULL;
+    do{
+        int newlineOffset = indexOfNewLine(assembly_file) + 1;
         void* buffer = calloc(1, newlineOffset);
+
+        if(buffer == NULL){
+            printf("Unable to allocate memory for program usage...\n");
+            exit(0);
+        }
 
         // rewind file position indicator (*newlineOffset times*) so buffer can consume all characters inbetween using fgets()
         // Reference of negative offsets to move file pointer backwards
@@ -33,9 +39,11 @@ int main(int argc, char *argv[])
 
         // as a side effect, fast-forward file position indicator will be done by fgets() so we can proceed to next line without being infinitely stuck reading first line
         fgets((char*)buffer, newlineOffset, assembly_file);
-        printf("Buffer: %s\n",(char*)buffer);
-    }
+        printf("Buffer: %s\n", (char*)buffer);
+    //FIXME: loop is not breaking!
+    } while(fgetc(assembly_file) != EOF);
 
+    free(buffer);
     fclose(assembly_file);
     exit(0);
 
