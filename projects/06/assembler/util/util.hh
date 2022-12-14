d58be749448b93311f3dbdfb306442d38c003fa5
Kesler Mathieu <kmat5@icloud.com>
2022-12-14 15:40:51 -0500

    add comment

--- projects/06/assembler/util/util.hh
+++ projects/06/assembler/util/util.hh
@@ -1,6 +1,8 @@
 #include <vector>
 
+// for A-instruction @value commands
 char* decimal_to_binary(int dec);
+
 char* strip_leading_and_trailing_whitespace(char* command);
 vector<char*> split(char* command, char delimiter);
 
